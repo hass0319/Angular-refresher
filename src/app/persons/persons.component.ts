a2aea6f80e811012d065809d4e2ae2dc6c5d9637
Amro Hassan <79529406+hass0319@users.noreply.github.com>
2022-10-14 17:46:57 +0200

    adding first component

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, Input } from "@angular/core";
 
 @Component({
   selector: 'app-persons',
@@ -6,4 +6,8 @@ import { Component } from "@angular/core";
 
 })
 
-export class PersonsComponent { }
+export class PersonsComponent {
+  @Input() personList: string[];
+  // persons: string[]=['Max', 'Manuel', 'Anna']
+  // personList: string[];
+}
